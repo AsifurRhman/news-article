21616fa2501485fbc2f222671ef5173f65303f48
Sourav123-sys <khansourav58@gmail.com>
2023-12-28 14:55:24 +0600

    thumbnail add and remove done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["example.com","lh3.googleusercontent.com","avatars.githubusercontent.com"],
+        domains: ["example.com",
+        "lh3.googleusercontent.com",
+            "avatars.githubusercontent.com",
+            "res.cloudinary.com"],
     }
 }
 
